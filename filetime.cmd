bb8d30b89e2503aa6679b03fd47c0bf163601cbf
FrankWestlake <frank.westlake@gmail.com>
2013-03-29 13:24:55 -0700

    2013-03-29

--- filetime.cmd
+++ filetime.cmd
@@ -1,6 +1,7 @@
 :: BEGIN SCRIPT :::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: filetime.cmd
 :: Calculates the 64-bit integer NTFS filetime of the file.
+:: WARNING: DO NOT USE AFTER 8406-02-09 04:31:39.9999999.
 :: From the desk of Frank P. Westlake, 2013-03-22
 :: Requires WMIC.exe.
 :: The purpose of this script is to distribute the subroutine and
@@ -28,6 +29,8 @@ If DEFINED $help (
   Echo   file      The name of the file to read the time attribute of.
   Echo;
   Echo If /ENV is not specified then the result will be printed.
+  Echo;
+  Echo WARNING: DO NOT USE AFTER 8406-02-09 04:31:39.9999999.
   EXIT /B 0
 )
 :args
@@ -61,12 +64,19 @@ If DEFINED $env (
   Call :filetime  $env  %$file% %$which%
   Echo;!$env!
 )
+If %ErrorLevel% NEQ 0 (
+  For /F "delims=" %%a in ('net HELPMSG %ErrorLevel%') Do (
+    Echo;!$ME!: %%a
+  )
+)
 Goto :EOF
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :filetime <variable name for result> <filename> [/c | /a | /w]
 :: From the desk of Frank P. Westlake, 2013-03-22
 :: Calculates the 64-bit integer NTFS filetime of the file.
+:: WARNING: DO NOT USE AFTER 8406-02-09 04:31:39.9999999.
+:: A filetime of 0 indicates an error condition; check the ERRORLEVEL.
 :: PARAMETERS ('+'=required, '-'=optional):
 :: + %1 The name of the variable to set the filetime into.
 :: + %2 The name of the file.
@@ -77,6 +87,7 @@ Goto :EOF
 :: Requires WMIC.exe.
 SetLocal EnableExtensions EnableDelayedExpansion
 For /F "tokens=1 delims==" %%a in ('Set "$" 2^>NUL:') Do Set "%%a="
+Set /A "$error=0"
 Set "$name=%~f2"
        If /I "%~3" EQU "/C" ( Set "$which=CreationDate"
 ) Else If /I "%~3" EQU "/A" ( Set "$which=LastAccessed"
@@ -93,25 +104,30 @@ For /F "tokens=1,2 delims==" %%a in (
     Set /A "$ft=$d+(153*$m+2)/5+365*$y+$y/4-$y/100+$y/400+58"
     Set /A "$s=1!$:~8,2! -100, $s=1!$:~10,2!-100+$s*60+$b*-1, $s=1!$:~12,2!-100+60*$s"
     If $s LSS 0 (Set /A "$ft-=1, $s+=24*60*60")
-    Set /A "$ft=$ft*24*6*6"
-    Set    "$ft=!$ft!00"
-    For %%A in (0 1 2 3 4 5 6 7 8 9) Do Set "$ft=!$ft:%%A= %%A!"
-    For %%A in (!$ft!) Do Set "$tf=%%A !$tf!"
-    For %%A in (0 1 2 3 4 5 6 7 8 9) Do Set "$s=!$s:%%A= %%A!"
-    For %%A in (!$s!) Do Set "$ts=%%A!$ts!"
-    Set "$ft="
-    Set /A "$carry=0, $k=0"
-    For %%n in (!$tf!) Do (
-      For %%k in (!$k!) Do Set "$nB=!$ts:~%%k,1!"
-      Set /a "$n=%%n+$nB+$carry"
-      Set /A "$carry=0, $k+=1"
-      If !$n! GEQ 10 (Set /A "$n-=10, $carry=1")
-      Set "$ft=!$n!!$ft!"
+    If !$ft! GEQ 2485514 (
+      REM OVERFLOW: "2485514 * 24 * 6 * 6 = 0x800001C0"
+      Set /A "$error=534, $ft=0"
+    ) Else (
+      Set /A "$ft=$ft*24*6*6"
+      Set    "$ft=!$ft!00"
+      For %%A in (0 1 2 3 4 5 6 7 8 9) Do Set "$ft=!$ft:%%A= %%A!"
+      For %%A in (!$ft!) Do Set "$tf=%%A !$tf!"
+      For %%A in (0 1 2 3 4 5 6 7 8 9) Do Set "$s=!$s:%%A= %%A!"
+      For %%A in (!$s!) Do Set "$ts=%%A!$ts!"
+      Set "$ft="
+      Set /A "$carry=0, $k=0"
+      For %%n in (!$tf!) Do (
+        For %%k in (!$k!) Do Set "$nB=!$ts:~%%k,1!"
+        Set /a "$n=%%n+$nB+$carry"
+        Set /A "$carry=0, $k+=1"
+        If !$n! GEQ 10 (Set /A "$n-=10, $carry=1")
+        Set "$ft=!$n!!$ft!"
+      )
+      If !$carry! EQU 1 Set "$ft=1!$ft!"
+      Set    "$ft=!$ft!!$:~15,6!0"
     )
-    If !$carry! EQU 1 Set "$ft=1!$ft!"
-    Set    "$ft=!$ft!!$:~15,6!0"
   )
 )
-EndLocal & Set "%~1=%$ft%"
+EndLocal & Set "%~1=%$ft%" & EXIT /B %$error%
 Goto :EOF
 :: END SCRIPT ::::::::::::::::::::::::::::::::::::::::::::::::::::
